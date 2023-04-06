2afff8f728bf09da391f0a8de1e7f450c1052611
Mehdi Zarouri <m.zarouri59@hotmail.fr>
2023-04-06 09:43:25 +0200

    premier commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('details_btn');
+const info = document.getElementById('info');
+const close_btn = document.getElementById('close');
+
+
+btn.onclick = function() {
+    
+}
+
