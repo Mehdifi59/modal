5a9f89c043069b8ff1a3b680cba87a590064221a
Mehdi Zarouri <m.zarouri59@hotmail.fr>
2023-04-06 10:05:24 +0200

    finalisation du modal

--- js/index.js
+++ js/index.js
@@ -3,7 +3,14 @@ const info = document.getElementById('info');
 const close_btn = document.getElementById('close');
 
 
+
 btn.onclick = function() {
-    
+    info.style.display = 'block';
 }
 
+close_btn.addEventListener('click', function(){
+
+    info.style.display = 'none';
+
+
+})
